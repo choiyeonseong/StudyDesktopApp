709a5665dbcea46184924cf8b04e8ab339bedc4d
choiyeonseong <am3251@naver.com>
2021-03-16 17:27:27 +0900

    0316 BookRentalShop

---
+++ WinformApp/WinFormAdvancedBank/BookRentalShopApp/FrmLogin.cs
@@ -0,0 +1,69 @@
+﻿using MetroFramework;
+using MetroFramework.Forms;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace BookRentalShopApp
+{
+    public partial class FrmLogin : MetroForm
+    {
+        public FrmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnLogin_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show("로그인 처리");
+            if (string.IsNullOrEmpty(TxtUserId.Text) || string.IsNullOrEmpty(TxtPassword.Text))
+            {
+                MetroMessageBox.Show(this, "아이디/패스워드를 입력하세요!", "오류",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error, Height);
+                return;
+            }
+
+            try
+            {
+                // SqlConnection 연결
+                using (SqlConnection conn = new SqlConnection(Helper.Common.ConnString))
+                {
+                    if (conn.State == ConnectionState.Closed) conn.Open();
+
+                    // sqlCommand 생성
+                    SqlCommand cmd = new SqlCommand();
+
+                    // SQL Injection 해킹을 방지하기 위해 사용
+                    SqlParameter param;
+
+                    // SqlDataReader 실행(1)
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    
+                    // reader로 처리...
+                }
+            }
+            catch (Exception ex)
+            {
+                MetroMessageBox.Show(this, $"Error : {ex.Message}", "오류",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error, Height);
+                return;
+            }
+        }
+
+        private void BtnCancel_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);    // 완전 종료(부모창 까지)
+        }
+
+        private void TxtUserId_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13) TxtPassword.Focus();
+        }
+
+        private void TxtPassword_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13) BtnLogin_Click(sender, e);
+        }
+    }
+}
---
+++ WinformApp/WinFormAdvancedBank/BookRentalShopApp/FrmMain.cs
@@ -0,0 +1,25 @@
+﻿using MetroFramework.Forms;
+using System;
+
+namespace BookRentalShopApp
+{
+    public partial class FrmMain : MetroForm
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FrmMain_Shown(object sender, EventArgs e)
+        {
+            FrmLogin frm = new FrmLogin();
+            frm.ShowDialog();
+        }
+    }
+}
