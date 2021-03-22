82305768b19f7ad1c781333903f67ea2e98a8f17
choiyeonseong <am3251@naver.com>
2021-03-22 12:32:35 +0900

    0322 binding

---
+++ WPFApp/WpfAdvBank/WpfPracticeApp/BusinessLogic/Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace WpfPracticeApp.BusinessLogic
+{
+    public class Car
+    {
+        public double Speed { get; set; }   // 속성(프로퍼티) : 대문자로 시작 
+        public Color MainColor { get; set; }  // 키워드는 중복사용 안하는게 좋음
+        public Human Driver { get; set; }
+    }
+    public class Human
+    {
+        public bool HasDriveLicense { get; set; }
+    }
+}
